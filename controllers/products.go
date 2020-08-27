3c65289c739d987ed8b348b257fecdbcc68f90af
Gigih Hadi P <hadigigih@gmail.com>
2020-08-27 20:02:42 +0700

    [ADD] crud product

--- controllers/products.go
+++ controllers/products.go
@@ -5,20 +5,20 @@ import (
 	"github.com/gin-gonic/gin"
 
 	. "rest-api-gin/models"
-	"rest-api-gin/config"
+	// "rest-api-gin/config"
 	. "rest-api-gin/helpers"
 	"strconv"
 	// "fmt"
 	// "reflect"
 )
 
-var db = config.ConnectDB()
+// var db = config.ConnectDB()
 var p []Products
 
-func AddProduct(c *gin.Context){
-	name := c.PostForm("name")
-	category, _ := strconv.ParseUint(c.PostForm("id_category"),10,32)
-	brand, _ := strconv.ParseUint(c.PostForm("id_brand"),10,32)
+func AddProduct(ctx *gin.Context){
+	name := ctx.PostForm("name")
+	category, _ := strconv.ParseUint(ctx.PostForm("id_category"),10,32)
+	brand, _ := strconv.ParseUint(ctx.PostForm("id_brand"),10,32)
 
 	product := Products{
 		Name: name, 
@@ -28,41 +28,41 @@ func AddProduct(c *gin.Context){
 
 	result := db.Create(&product); 
 	if result.Error != nil{
-		ResponseJSON(c, 998, result.Error)
+		ResponseJSON(ctx, 998, result.Error)
 	}else{
 		res := make(map[string]uint)
 		res["id"]=product.ID
-		ResponseJSON(c, 100, res)
+		ResponseJSON(ctx, 100, res)
 	}
 }
 
-func FindProduct(c *gin.Context) {
+func FindProduct(ctx *gin.Context) {
 	result := db.Find(&p)
 
 	if result.Error != nil {
-		ResponseJSON(c, 998, result.Error)
+		ResponseJSON(ctx, 998, result.Error)
 	} else {
-		ResponseJSON(c, 100, result.Value)
+		ResponseJSON(ctx, 100, result.Value)
 	}
 }
 
-func FindProductByID(c *gin.Context){
-	id := c.Param("id")
+func FindProductByID(ctx *gin.Context){
+	id := ctx.Param("id")
 	result := db.Find(&p,id)
 
 	if result.Error != nil {
-		ResponseJSON(c, 998, result.Error)
+		ResponseJSON(ctx, 998, result.Error)
 	} else {
-		ResponseJSON(c, 100, result.Value)
+		ResponseJSON(ctx, 100, result.Value)
 	}
 
 }
 
-func EditProduct(c *gin.Context){
-	id := c.Param("id")
+func EditProduct(ctx *gin.Context){
+	id := ctx.Param("id")
 	// name := c.PostForm("name")
-	id_category,_ := strconv.ParseUint(c.PostForm("id_category"),10,32)
-	id_brand,_ := strconv.ParseUint(c.PostForm("id_brand"),10,32)
+	id_category,_ := strconv.ParseUint(ctx.PostForm("id_category"),10,32)
+	id_brand,_ := strconv.ParseUint(ctx.PostForm("id_brand"),10,32)
 
 	// db.First(&p, uint(id))
 	result := db.Model(&p).Where(id).Updates(map[string]interface{}{
@@ -71,20 +71,20 @@ func EditProduct(c *gin.Context){
 	})
 
 	if result.Error != nil {
-		ResponseJSON(c, 998, result.Error)
+		ResponseJSON(ctx, 998, result.Error)
 	}else{
-		ResponseJSON(c, 100, id)
+		ResponseJSON(ctx, 100, id)
 	}
 }
 
-func DeleteProduct(c *gin.Context){
-	id := c.Param("id")
+func DeleteProduct(ctx *gin.Context){
+	id := ctx.Param("id")
 
 	db.Model(&p).Where(id).Update("is_deleted",1)
 	result := db.Where(id).Delete(&Products{})
 	if result.Error != nil {
-		ResponseJSON(c, 998, result.Error)
+		ResponseJSON(ctx, 998, result.Error)
 	} else {
-		ResponseJSON(c, 100, result)
+		ResponseJSON(ctx, 100, result)
 	}
 }
