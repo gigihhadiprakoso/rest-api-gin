90155661e593f81f85ce5be74461fb388a33452c
Gigih Hadi P <hadigigih@gmail.com>
2020-09-19 21:55:36 +0700

    [ADD] category

--- controllers/cateogries.go
+++ controllers/cateogries.go
@@ -1,20 +1,79 @@
 package controllers
 
 import (
-	// "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-	// "rest-api-gin/models"
+	. "rest-api-gin/models"
 	// "rest-api-gin/config"
-	// . "rest-api-gin/helpers"
+	. "rest-api-gin/helpers"
 	// "strconv"
 )
 
-// var db = config.ConnectDB()
+var c []Categories
 
-func AddCategory(){
+func AddCategory(ctx *gin.Context){
+	name := ctx.PostForm("name")
+
+	category := Categories{
+		Name: name,
+		CompanyID: ID_Company,
+	}
+	
+	result := db.Create(&category); 
+	if result.Error != nil{
+		ResponseJSON(ctx, 998, result.Error)
+	}else{
+		res := make(map[string]uint)
+		res["id"]=category.ID
+		ResponseJSON(ctx, 100, res)
+	}
+}
+
+func FindCategory(ctx *gin.Context){
+	result := db.Find(&c,"company_id=?",ID_Company)
+
+	if result.Error != nil {
+		ResponseJSON(ctx, 998, result.Error)
+	} else {
+		ResponseJSON(ctx, 100, result.Value)
+	}
+}
+
+func FindCategoryByID(ctx *gin.Context){
+	id := ctx.Param("id")
+	result := db.Find(&c,id)
+
+	if result.Error != nil {
+		ResponseJSON(ctx, 998, result.Error)
+	} else {
+		ResponseJSON(ctx, 100, result.Value)
+	}
 
 }
 
-func FindCategory(){
+func EditCategory(ctx *gin.Context){
+	id := ctx.Param("id")
+	name := ctx.PostForm("name")
 
+	result := db.Model(&c).Where(id).Updates(map[string]interface{}{
+		"name":name,
+	})
+
+	if result.Error != nil {
+		ResponseJSON(ctx, 998, result.Error)
+	}else{
+		ResponseJSON(ctx, 100, id)
+	}
+}
+
+func DeleteCategory(ctx *gin.Context){
+	id := ctx.Param("id")
+
+	db.Model(&c).Where(id).Update("is_deleted",1)
+	result := db.Where(id).Delete(&Categories{})
+	if result.Error != nil {
+		ResponseJSON(ctx, 998, result.Error)
+	} else {
+		ResponseJSON(ctx, 100, result)
+	}
 }
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	// "fmt"
 	"rest-api-gin/config"
 	"github.com/gin-gonic/gin"
-	// . "rest-api-gin/models"
+	. "rest-api-gin/models"
 	"rest-api-gin/controllers"
 	// "rest-api-gin/seed"
-	// "log"
+	. "rest-api-gin/helpers"
+	jwt "github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 var db = config.ConnectDB()
@@ -15,6 +16,11 @@ var db = config.ConnectDB()
 func main() {
 	// seed.Load(db);
 	router := gin.Default()
+	
+	router.POST("register",controllers.Register)
+	router.POST("login",controllers.Login)
+
+	router.Use(authMiddleware())
 
 	product := router.Group("product")
 	{
@@ -25,15 +31,40 @@ func main() {
 		product.PUT(":id",controllers.EditProduct)
 	}
 
-	/*category := router.Group("category")
+	category := router.Group("category")
 	{
 		category.POST("add",controllers.AddCategory)
 		category.GET("",controllers.FindCategory)
-	}*/
-
-
-	router.POST("register",controllers.Register)
-	router.POST("login",controllers.Login)
+		category.GET(":id",controllers.FindCategoryByID)
+		category.PUT(":id",controllers.EditCategory)
+		category.DELETE(":id",controllers.DeleteCategory)
+	}
 
 	router.Run()
+}
+
+func authMiddleware() gin.HandlerFunc{
+	
+	return func(ctx *gin.Context){
+		headerAuthorization := ctx.Request.Header.Get("Authorization")
+		token := ExtractToken(headerAuthorization)
+
+		if token == "" {
+			ResponseJSON(ctx, 996, "")
+			return
+		}
+
+		claims := Claims{}
+		tokenDecoded,_ := jwt.ParseWithClaims(token, &claims, func(tokenDecoded *jwt.Token) (interface{}, error) {
+			return []byte("gigih-rupawan"), nil
+		})
+
+		if !tokenDecoded.Valid || claims.ExpiresAt < time.Now().Unix() {
+			ResponseJSON(ctx, 995, "")
+			return
+		}
+
+		controllers.ID_Company = claims.CompanyID
+		ctx.Next()
+	}
 }
